c69a14a664cf36b90484200045c07f0dc163dd61
Miia Klingstedt <miia@restfuldesign.com>
2025-06-05 12:56:24 +0200

    Image slider to pp

--- assets/shop.js
+++ assets/shop.js
@@ -6,17 +6,3 @@ document.addEventListener('click', function(e) {
     details.forEach(f => !f.contains(e.target) ? f.removeAttribute('open') : '');
   }
 })
-
-document.addEventListener('DOMContentLoaded', function() {
-  const mainImage = document.getElementById('main-image');
-  const thumbnails = document.querySelectorAll('.thumb');
-
-  thumbnails.forEach(thumb => {
-    thumb.addEventListener('click', function() {
-      const largeImageSrc = this.dataset.large;
-      const largeImageAlt = this.alt;
-      mainImage.src = largeImageSrc;
-      mainImage.alt = largeImageAlt;
-    });
-  });
-});
