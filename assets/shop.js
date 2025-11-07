b05e4d5bd5c6a83d8f4c811b80ff387edfec09ba
anders elo <andelo@kaerus.com>
2025-11-07 17:21:05 +0100

    updated to alpine 3.15.1. cart_update using ajax(fetch). rearranged some theme settings.

--- assets/shop.js
+++ assets/shop.js
@@ -6,3 +6,28 @@ document.addEventListener('click', function(e) {
     details.forEach(f => !f.contains(e.target) ? f.removeAttribute('open') : '');
   }
 })
+
+window.shop = {
+    quantityAdjust(item,quantity) {
+        if(quantity < 0) return;
+        
+        let update = {items:{}};
+        update.items[item] = quantity;
+        
+        fetch("/cart_update", {
+            method: "PUT",
+            body: JSON.stringify(update),
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            },
+        }).then(res => {
+            if (!res.ok) throw "failed to update quantity";
+            location.reload();
+        }).catch(err => {
+            console.error(err);
+            alert(err);
+        });
+    }
+}
